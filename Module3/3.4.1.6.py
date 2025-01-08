4d1fc088f7e96b4a9d3b1c0c14427b1fdf764e56
Irakli <krineri12@gmail.com>
2025-01-08 22:57:52 +0400

    upd

--- Module3/3.4.1.6.py
+++ Module3/3.4.1.6.py
@@ -2,9 +2,12 @@ hat_list = [1, 2, 3, 4, 5]  # This is an existing list of numbers hidden in the 
 
 # Step 1: write a line of code that prompts the user
 # to replace the middle number with an integer number entered by the user.
+midd_int = int(input("enter number:"))
+hat_list.remove(hat_list[len(hat_list)-1])
+hat_list.insert(2,midd_int)
 
 # Step 2: write a line of code that removes the last element from the list.
-
+hat_list.pop()
 # Step 3: write a line of code that prints the length of the existing list.
-
+print(len(hat_list))
 print(hat_list)
